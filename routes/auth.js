8209bdfb666965e9c648e27d4ba1cbf957b71a8a
astefanovic <astefanovic101@gmail.com>
2017-11-05 20:40:36 +1100

    Added db and schemas, added auth packages

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ var express = require("express");
 // Router to declare the routes and export at the end
 var router = express.Router();
 
-router.get("/", function(req, res) {
+router.get("/login", function(req, res) {
     res.render("timesheets", { user: req.user });
 });
 
